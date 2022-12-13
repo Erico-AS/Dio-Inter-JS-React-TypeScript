671107ecfdc24c191e2ad753778bde25221b3e38
Erico-AS <ericoaugustosstj@gmail.com>
2022-12-13 20:30:08 -0300

    React

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,30 @@
-import { Link } from 'react-router-dom';
+import { Button } from '../../components/Button';
+import { Header } from "../../components/Header";
+import { Container, TextContent, Title, TitleHighlight } from "./styles"
+import Banner from "../../assets/b.png"
 
 const Home = () => {
     return (<>
-        <h1>Página inicial</h1>
-        <Link to="/login">Login</Link>
+        <Header>Home</Header>
+        <Container>
+            <div>
+                <Title>
+                    <TitleHighlight> 
+                        Implemente <br />
+                    </TitleHighlight>
+                    o seu futuro global agora!
+                </Title>
+
+                <TextContent>
+                    Domine as tecnologias ultilizadas pelas empresas mais inovadoras do mundo e
+                    encare seu novo desafio profissional, evoluindo em comunidade com os melhores experts.
+                </TextContent>
+            </div>
+
+            <div>
+                <img src={Banner} alt='Imagem principal' />
+            </div>
+        </Container>
     </>)
 }
 
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,36 @@
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+import { MdEmail, MdLock } from "react-icons/md"
+import { Column, Container, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper } from "./styles"
+import { Header } from "../../components/Header";
+
+
+export function Login() {
+    return (<>
+        <Header />
+        <Container>
+            <Column>
+                <Title>
+                    Domine as tecnologias ultilizadas pelas empresas mais inovadoras do mundo e
+                    encare seu novo desafio profissional, evoluindo em comunidade com os melhores experts.
+                </Title>
+            </Column>
+
+            <Column>
+                <Wrapper>
+                    <TitleLogin>Faça seu cadastro</TitleLogin>
+                    <SubtitleLogin>Faça seu login e make the change</SubtitleLogin>
+                    <form>
+                        <Input placeholder='E-mail' leftIcon={<MdEmail/>}/>
+                        <Input placeholder='Senha' type='password' leftIcon={<MdLock/>}/>
+                        <Button title='Entrar' variant="secondary"/>
+                    </form>
+                    <Row>
+                        <EsqueciText>Esqueci minha senha</EsqueciText>
+                        <CriarText>Criar conta</CriarText>
+                    </Row>
+                </Wrapper>
+            </Column>
+        </Container>
+    </>)
+}
