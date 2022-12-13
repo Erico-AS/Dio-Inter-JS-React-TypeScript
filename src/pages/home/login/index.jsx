2c560c403db634ef9b02ef44ee89b6672adb1aee
Erico-AS <ericoaugustosstj@gmail.com>
2022-12-13 20:20:10 -0300

    React

---
+++ src/pages/home/login/index.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    return (<>
+        <h1>Página de Login</h1>
+        <Link to="/">Voltar para home</Link>
+    </>)
+}
+
+export {Login}
